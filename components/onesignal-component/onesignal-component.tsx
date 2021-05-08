67cc257e02e3cd3d7fc9a357bac191e0010866dd
Diego Gonzalez <diego.ags04@gmail.com>
2021-05-08 00:20:48 -0400

    Removed onesignal button

--- components/onesignal-component/onesignal-component.tsx
+++ components/onesignal-component/onesignal-component.tsx
@@ -25,9 +25,6 @@ const OnesignalComponent: React.FC = () => {
 		const OneSignal = window.OneSignal || [];
 		const initConfig = {
 			appId: process.env.NEXT_PUBLIC_ONESIGNAL_KEY,
-			notifyButton: {
-				enable: true,
-			},
 		};
 		OneSignal.push(function () {
 			OneSignal.SERVICE_WORKER_PARAM = { scope: '/push/onesignal/' };
